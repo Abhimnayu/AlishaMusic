018bb800434d32b86b9c4bfc9b9bc16ce4efdae8
Abhimnayu <106476391+Abhimnayu@users.noreply.github.com>
2022-06-17 09:31:40 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -22,7 +22,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += ' ' + str(i)
     print(query)
-    m = message.reply("𝐅𝐢𝐧𝐝𝐢𝐧𝐠 💫 𝐓𝐡𝐞 𝐒𝐨𝐧𝐠 🎧 𝐅𝐫𝐨𝐦 ★𝐓𝐌𝐂♪♪𝐌𝐔𝐒𝐈𝐂'𝐗✌︎✌︎★❤️ 𝐒𝐞𝐫𝐯𝐞𝐫 🌎...")
+    m = message.reply("Fɪɴᴅɪɴɢ 💫 ᴛʜᴇ sᴏɴɢ 🎧 ғʀᴏᴍ ★Aʟɪsʜᴀ★❤️ Sᴇʀᴠᴇʀ 🌎...")
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -41,17 +41,17 @@ def song(client, message):
 
     except Exception as e:
         m.edit(
-            "𝐒𝐨𝐧𝐠 🥀 𝐍𝐨𝐭 😔 𝐅𝐨𝐮𝐧𝐝."
+            "Sᴏɴɢ 🥀 ɴᴏᴛ 😔 ғᴏᴜɴᴅ."
         )
         print(str(e))
         return
-    m.edit("𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 ✨ 𝐒𝐨𝐧𝐠 🎸 𝐅𝐫𝐨𝐦 ★𝐓𝐌𝐂♪♪𝐌𝐔𝐒𝐈𝐂'𝐗✌︎✌︎★🥀 𝐒𝐞𝐫𝐯𝐞𝐫 🌎...")
+    m.edit("Dᴏᴡɴʟᴏᴀᴅ ✨ sᴏɴɢ 🎸 ғʀᴏᴍ ★Aʟɪsʜᴀ★🥀 sᴇʀᴠᴇʀ 🌎...")
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "**🎵 𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐝 𝐁𝐲 :- ✨ [༒︎★•亗『𝐊𝐀𝐓𝐈𝐋』亗•★](https://t.me/TERA_BAAP_KATIL) ❤️**"
+        rep = "**🎵 Uᴘʟᴏᴀᴅ ʙʏ :- ✨ [Aʟɪsʜᴀ sᴇʀᴠᴇʀ](https://t.me/AlishaSupport) ❤️**"
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
@@ -59,7 +59,7 @@ def song(client, message):
         message.reply_audio(audio_file, caption=rep, thumb=thumb_name, parse_mode='md', title=title, duration=dur)
         m.delete()
     except Exception as e:
-        m.edit("**𝐘𝐨𝐮𝐭𝐮𝐛𝐞  𝐄𝐫𝐫𝐨𝐫 ❌ 𝐂𝐨𝐧𝐭𝐚𝐜𝐭 𝐎𝐰𝐧𝐞𝐫 🥀 [★•亗『𝐊𝐀𝐓𝐈𝐋』亗•★](https://t.me/tera_baap_katil)❤️ to Fix it !!**")
+        m.edit("**Yᴏᴜᴛᴜʙᴇ ᴇʀʀᴏʀ ❌ Cᴏɴᴛᴀᴄᴛ ᴏᴡɴᴇʀ🥀 [Aʙʜɪᴍᴀɴʏᴜ Sɪɴɢʜ Rᴀɴᴀᴡᴀᴛ](https://t.me/VeNom_Hai_HuM)❤️ to Fix it !!**")
         print(e)
 
     try:
